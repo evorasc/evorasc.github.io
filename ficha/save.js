a53c8118bce4b363bf1723d2546f2e9dad63a9fa
hudsonmacedo <hudsonmacedo14@gmail.com>
2017-04-09 23:48:32 -0300

    missing the last section

--- ficha/save.js
+++ ficha/save.js
@@ -1,5 +1,5 @@
 // compress and uncompress the card
-u('.toggle').on('click', function(){
+u('.toggle, h2, label.select').on('click', function(){
 	u(this).parent().toggleClass("compressed");
 })
 
@@ -30,11 +30,11 @@ u('.mais').on('click', function(){
 u('.menos').on('click', function(){
 	var valor = u(this).parent().children('.ponto').text();
 	var valor = parseInt(valor) - 1;
-	if (valor < 1) {
-		u(this).parent().children('.ponto').text("0");
-	} else {
+	// if (valor < 1) {
+		// u(this).parent().children('.ponto').text("0");
+	// } else {
 		u(this).parent().children('.ponto').text(valor);
-	}
+	// }
 })
 // save experience points
 u('.menos, .mais').on('click', function(){
@@ -43,7 +43,20 @@ u('.menos, .mais').on('click', function(){
 	localStorage.setItem(id, valor);
 })
 
-// save the value of each experience
+//selecting option
+u('p.option').on('click',function(){
+	u(this).toggleClass('chosen');
+	//Showing the chosen option below
+	var option = u(this).data('option')
+	u('#'+option).parent().parent().parent().toggleClass('hide')
+	//saving choices
+	var id = u(this).attr("id");
+	if (u(this).is('.chosen')) {
+		localStorage.setItem(id, "chosen");	
+	} else {
+	localStorage.setItem(id, "");
+	}
+})
 
 
 // fill the field with the saved values
@@ -55,6 +68,14 @@ function fill() {
 		document.getElementById(id).value = valor;
 	})
 
+	// page title = character title
+	var personagem = localStorage.getItem("personagem");
+	if (personagem == "") {
+		u('#charTitle').text("ficha de personagem");
+	} else {
+		u('#charTitle').text("ficha de "+personagem);
+	}
+
 	// fill experience points
 	u('.card').find('.ponto').each(function(node, i){
 		var id = u(node).attr("id");
@@ -63,6 +84,18 @@ function fill() {
 			document.getElementById(id).innerHTML = "0";
 		} else {
 			document.getElementById(id).innerHTML = valor;
+		}
+	})
+
+	// fill advantege points
+	u('.vantagens').find('.option').each(function(node, i){
+		var id = u(node).attr("id");
+		var valor = localStorage.getItem(id);
+		if (valor == null || valor == "NaN" || valor == "") {
+		} else {
+			u(node).toggleClass(valor)
+			var option = u(node).data('option')
+			u('#'+option).parent().parent().parent().toggleClass('hide')
 		}
 	})
 }
